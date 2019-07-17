91177c1d05fceeb8ddb41db900c04eb52ce234fb
fumeco <48030503+fumeco@users.noreply.github.com>
2019-07-17 16:52:27 -0300

    calculadora Javascript AIPR2019 V1
    feito em sala de aula com o proff:tarcisio

---
+++ Calculadora/js/meuJavascript.js
@@ -0,0 +1,28 @@
+function minhaFunção() {
+    //O uso mais comum para saida de dados
+    document.getElementById("demo")
+    .innerHTML = "parágrafo mudado.";
+
+    //window.elert("este é o alerta!");
+    //Uso mais comum para depuração de codigo
+    console.log("só apareçp com F12");
+}
+
+function minhaFunção2() {
+    if (document.getElementById("demo1").innerText.length === 0){
+
+    document.getElementById("demo1")
+    .innerHTML = "Olá Dollinho";
+
+    document.getElementById("demo2")
+    .innerHTML = "O melhor do Brasil. O melhor!";
+}else{
+    //senão, apaga p seu texto
+    document.getElementById("demo1").innerHTML = "";
+
+    document.getElementById("demo2").innerHTML = "";
+
+    document.querySelector("#div").style
+}
+
+}
